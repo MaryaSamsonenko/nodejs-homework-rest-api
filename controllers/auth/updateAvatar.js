93fa3d8e660092244039cd2d622823fb088600ad
Marya Samsonenko <samashushka7@gmail.com>
2022-11-02 19:22:12 +0200

    add jimp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,21 +1,30 @@
 const fs = require("fs/promises");
 const path = require("path");
+const Jimp = require("jimp");
 
 const { User } = require("../../models/user");
 
 const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
-const updateAvatar = async (req, res) => {
+const updateAvatar = async (req, res, next) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
-  const extention = originalname.split(".").pop();
-  const filename = `${_id}.${extention}`;
-  const resultUpload = path.join(avatarDir, filename);
-  await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
+  try {
+    const image = await Jimp.read(tempUpload);
+    image.resize(250, 250);
+    image.write(tempUpload);
+    const extention = originalname.split(".").pop();
+    const filename = `${_id}.${extention}`;
+    const resultUpload = path.join(avatarDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
 
-  res.status(200).json({ avatarURL });
+    res.status(200).json({ avatarURL });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    next(error.message);
+  }
 };
 
 module.exports = updateAvatar;
